48e4902bc919d4e22acbe83f41f16cfa4b51514e
Aleksandr Golomshtok <sgolomshtokm@gmail.com>
2026-05-05 00:20:46 +0300

    day counter

--- autoloads/time_manager.gd
+++ autoloads/time_manager.gd
@@ -5,25 +5,25 @@ signal clear_watered_tiles
 
 var save_path := "user://garden/game_data.cfg"
 
-
 func load_game():
 	var config = ConfigFile.new()
 	var err = config.load(save_path)
 	if err == OK:
-		return config.get_value("game", "plants")
+		return [config.get_value("game", "plants"), config.get_value("game", "day")]
 	else:
 		print("Save error code: ", err)
 
 
-func next_day():
+func next_day(day_counter):
 	plants_grow.emit(1, Vector2i(100000, 100000))
 	emit_signal("clear_watered_tiles")
-	save_all()
+	save_all(day_counter)
 
 
-func save_all():
+func save_all(day_counter):
 	var config := ConfigFile.new()
 	config.set_value("game", "plants", get_plants())
+	config.set_value("game", "day", day_counter)
 	var dir := save_path.get_base_dir()
 	DirAccess.make_dir_recursive_absolute(dir)
 	config.save(save_path)
